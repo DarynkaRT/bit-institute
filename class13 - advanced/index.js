777d373c1c01d750199bfd071bf7a85018d7236c
DarynkaRT <95662010+DarynkaRT@users.noreply.github.com>
2022-01-18 20:35:06 -0500

    class13 - advanced

---
+++ class13 - advanced/index.js
@@ -0,0 +1,33 @@
+const container = document.getElementById("container");
+container.innerHTML = "soy un div"
+container.classList
+
+const titlesArray= document.getElementsById("title")
+
+titlesArray[0].innerHTML = "soy un título";
+
+const subtitle = document.querySelector(".subtitles");
+const nodetext = document.createTextNode("hola soy un subtitulo");
+subtitle.appendChild(nodetext);
+
+const newcontainer = document.createElement("span")
+const h3 = document.createElement("h3");
+const content = document.createTextNode( text )
+h3.appendChild(content);
+newcontainer.appendChild(content);
+document.body.appendChild(newcontainer)
+
+
+
+
+
+
+
+
+
+const h5 = document.createElementNS("h5");
+const texth5node = document.createTextNode("soy un h5")
+h5.appendChild(texth5node)
+document.body.append(h5);
+const eventType = "db1clik";
+const 
